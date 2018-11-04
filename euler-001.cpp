3e6c131957a19f3e6e3e68dafb43bd56368fc8ba
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-11-04 11:37:33 +0100

    Add euler-001

---
+++ euler-001.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+const unsigned int SUM_OF_TRIPLET = 1000;
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9.  The sum of these multiples is 23.
+//
+//  Find the sum of all the multiples of 3 or 5 below 1000.
+//
+int main()
+{
+  unsigned int sum_of_multiples = 0;
+
+  for (auto i = 0; i < 1000; ++i)
+  {
+    if (i % 3 == 0 || i % 5 == 0) sum_of_multiples += i;
+  }
+
+  std::cout << sum_of_multiples << "\n";
+
+  return 0;
+}
