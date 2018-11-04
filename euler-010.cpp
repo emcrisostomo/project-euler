9180c76188f028f07586a98399a0a062f7ce24f4
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-11-04 19:55:42 +0100

    Refactor to use a lambda

--- euler-010.cpp
+++ euler-010.cpp
@@ -4,13 +4,6 @@
 
 const unsigned int upper_limit = 2'000'000;
 
-void prime_sum(unsigned long i, void *context)
-{
-  auto *sum_of_primes = static_cast<unsigned long *>(context);
-
-  *sum_of_primes += i;
-}
-
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 //
 // Find the sum of all the primes below two million.
@@ -19,7 +12,12 @@ int main()
 {
   unsigned long sum_of_primes = 0;
 
-  prime::sieve_of_erathostenes(upper_limit, &prime_sum, &sum_of_primes);
+  prime::sieve_of_erathostenes(upper_limit,
+                               [](unsigned long i, void *context)
+                               {
+                                 *static_cast<unsigned long *>(context) += i;
+                               },
+                               &sum_of_primes);
 
   std::cout << sum_of_primes << "\n";
   return 0;
