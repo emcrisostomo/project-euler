1a463e2fd6634059a4998cc9aa4e069fe25774ad
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-11-04 11:30:14 +0100

    Add euler-009

---
+++ euler-009.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+const unsigned int SUM_OF_TRIPLET = 1000;
+
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//
+//   a2 + b2 = c2
+//
+// For example, 32 + 42 = 9 + 16 = 25 = 52.
+//
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+//
+int main()
+{
+
+  for (auto a=0; a < SUM_OF_TRIPLET/3; ++a)
+  {
+    for (auto b=a+1; b < SUM_OF_TRIPLET/2; ++b)
+    {
+      auto c = SUM_OF_TRIPLET - a - b;
+
+      if (a * a + b * b == c * c)
+      {
+        std::cout << a * b * c << "\n";
+        return 0;
+      }
+    }
+  }
+
+  return 1;
+}
