3f10f537d5a25a9aaa3f8be471a13e14a7704b8b
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-11-04 19:49:48 +0100

    Refactor to use library function

--- euler-010.cpp
+++ euler-010.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <vector>
+#include "prime.h"
 
 const unsigned int upper_limit = 2'000'000;
+
+void prime_sum(unsigned long i, void *context)
+{
+  auto *sum_of_primes = static_cast<unsigned long *>(context);
+
+  *sum_of_primes += i;
+}
 
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 //
@@ -9,23 +17,9 @@ const unsigned int upper_limit = 2'000'000;
 //
 int main()
 {
-  std::vector<bool> primes(upper_limit, true);
-  primes[0] = false;
-  primes[1] = false;
-
   unsigned long sum_of_primes = 0;
 
-  for (unsigned long i = 2; i < primes.size(); ++i)
-  {
-    if (!primes[i])
-      continue;
-
-    sum_of_primes += i;
-    for (unsigned long j = i * i; j < primes.size(); j += i)
-    {
-      primes[j] = false;
-    }
-  }
+  prime::sieve_of_erathostenes(upper_limit, &prime_sum, &sum_of_primes);
 
   std::cout << sum_of_primes << "\n";
   return 0;
