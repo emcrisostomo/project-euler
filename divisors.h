930b7c8e4d3c3a21c57fe11d911a3544cb9439cf
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-11-25 00:37:29 +0100

    Move divisors:: function to template

--- divisors.h
+++ divisors.h
@@ -6,11 +6,36 @@
 #define PROJECT_EULER_DIVISORS_H
 
 #include <vector>
+#include <cmath>
 
 namespace divisors
 {
-  std::vector<unsigned int> proper_divisors_of(unsigned int n);
+  template<typename T>
+  std::vector<T> proper_divisors_of(T n);
 };
 
+template<typename T>
+std::vector<T> divisors::proper_divisors_of(T n)
+{
+  if (n == 0) return {};
+
+  std::vector<T> d;
+  d.push_back(1);
+  const auto s = static_cast<const T>(sqrt(n));
+
+  for (T i = 2; i <= s; ++i)
+  {
+    if (n % i == 0)
+    {
+      d.push_back(i);
+      d.push_back(n / i);
+    }
+  }
+
+  // sqrt(n) is inserted twice when n is a square number.
+  if (s * s == n) d.pop_back();
+
+  return d;
+}
 
 #endif //PROJECT_EULER_DIVISORS_H
