562a2de5ca1c760c776cdf3da3fedec026df85e6
Crisostomo, Enrico M <enrico.m.crisostomo@accenture.com>
2018-11-24 17:55:19 +0100

    Add euler-022.cpp

--- euler-022.cpp
+++ euler-022.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <fstream>
 #include <vector>
-#include <cmath>
+#include <algorithm>
 #include <numeric>
-#include "divisors.h"
+
+std::vector<std::string> parse_names(std::string line);
+void parse_token(const std::string& token,
+                 std::vector<std::string>& tokens);
+unsigned int value_of(std::string name);
 
 // Using names.txt, a 46K text file containing over five-thousand first names,
 // begin by sorting it into alphabetical order.  Then working out the
@@ -17,10 +21,87 @@
 //
 // What is the total of all the name scores in the file?
 //
-int main()
+int main(int argc, char *argv[])
 {
-  unsigned int sum{0};
+  if (argc != 2)
+  {
+    std::cerr << "Invalid number of arguments: " << argc << "\n";
+    return 1;
+  }
+
+  std::fstream infile(argv[1]);
+  std::string line;
+  std::vector<std::string> names;
+
+  while (std::getline(infile, line))
+  {
+    const auto& names_in_line = parse_names(line);
+    names.insert(names.end(), names_in_line.begin(), names_in_line.end());
+  }
+
+  std::sort(names.begin(), names.end());
+
+  std::vector<unsigned int> name_values;
+  std::for_each(names.begin(),
+                names.end(),
+                [&name_values](const auto& n)
+                {
+                  name_values.push_back(value_of(n));
+                });
+
+  for (auto i = 0; i < name_values.size(); ++i)
+  {
+    name_values[i] *= (i + 1);
+  }
+
+  unsigned int sum = std::accumulate(name_values.begin(), name_values.end(), 0u);
 
   std::cout << sum << "\n";
   return 0;
+}
+
+std::vector<std::string> parse_names(std::string line)
+{
+  // The line is a comma-separated list of quoted names.
+  const std::string delimiter = ",";
+  size_t pos = 0;
+  std::string token;
+  std::vector<std::string> tokens;
+
+  while ((pos = line.find(delimiter)) != std::string::npos)
+  {
+    token = line.substr(0, pos);
+
+    parse_token(token, tokens);
+    line.erase(0, pos + delimiter.length());
+  }
+
+  parse_token(line, tokens);
+
+  return tokens;
+}
+
+void parse_token(const std::string& token,
+                 std::vector<std::string>& tokens)
+{
+  if (token.size() <= 2)
+    throw std::runtime_error(
+      std::string("Unexpected data format of token: ")
+        .append(token));
+
+  tokens.push_back(token.substr(1, token.size() - 2));
+}
+
+unsigned int value_of(std::string name)
+{
+  unsigned int value{0};
+
+  std::for_each(name.begin(),
+                name.end(),
+                [&value](const auto& v)
+                {
+                  value += v - 'A' + 1;
+                });
+
+  return value;
 }
