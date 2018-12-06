f4874b0600a9b5ab218c10e9cf822fe6d7265515
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-12-06 11:44:21 +0100

    Add euler-027.cpp

--- euler-027.cpp
+++ euler-027.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "prime.h"
 
-unsigned int get_repeating_decimal_length(unsigned long prime);
-
 // Euler discovered the remarkable quadratic formula:
 //
-//   n2+n+41
+//   n^2+n+41
 //
 // It turns out that the formula will produce 40 primes for the consecutive
 // integer values 0≤n≤39.  However, when n=40, 402+40+41=40(40+1)+41 is
@@ -18,7 +16,7 @@ unsigned int get_repeating_decimal_length(unsigned long prime);
 //
 // Considering quadratics of the form:
 //
-//   n2+an+b, where |a|<1000 and |b|≤1000
+//   n^2+an+b, where |a|<1000 and |b|≤1000
 //
 //   where |n| is the modulus/absolute value of n
 //   e.g. |11|=11 and |−4|=4
@@ -29,7 +27,60 @@ unsigned int get_repeating_decimal_length(unsigned long prime);
 //
 int main()
 {
-  std::cout << "\n";
+  // Given f(n) = n^2 + an + b, we have:
+  //
+  //   f(0) = b
+  //   f(1) = 1 + a + b
+  //
+  // from which we infer that:
+  //
+  //   - b must be a prime number -> b is prime and > 0.
+  //   - 1 + a + b must be a prime number -> a is odd and a > -b
+  auto b_range = prime::get_primes_smaller_than(1000);
+  std::vector<long> a_range;
+  a_range.reserve(b_range.size() * 2);
+
+  for (auto iter = b_range.rbegin(); iter != b_range.rend(); ++iter)
+  {
+    a_range.push_back(*iter * -1);
+  }
+
+  for (auto& b : b_range)
+  {
+    a_range.push_back(b);
+  }
+
+  unsigned int consecutive_prime_count{0};
+  long coefficient_product{0};
+
+  for (auto b = 0; b < b_range.size(); ++b)
+  {
+    for (auto a = (a_range.size() - 1) / 2 - b; a < a_range.size(); ++a)
+    {
+      unsigned int n{0};
+
+      const auto& ac = a_range[a];
+      const auto& bc = b_range[b];
+
+      long f = n * n + ac * n + bc;
+      if (f < 0) f *= -1;
+
+      while (prime::is_prime(static_cast<unsigned long>(f)))
+      {
+        f = n * n + ac * n + bc;
+        if (f < 0) f *= -1;
+        ++n;
+      }
+
+      if (n > consecutive_prime_count)
+      {
+        consecutive_prime_count = n;
+        coefficient_product = a_range[a] * b_range[b];
+      }
+    }
+  }
+
+  std::cout << coefficient_product << "\n";
   return 0;
 }
 
