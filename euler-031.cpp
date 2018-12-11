8220edf786d6d103c26b82ce617ce0e6de28d947
Crisostomo, Enrico M <enrico.m.crisostomo@accenture.com>
2018-12-11 23:35:43 +0100

    Add test for euler-031.cpp

--- euler-031.cpp
+++ euler-031.cpp
@@ -14,7 +14,7 @@
 //
 int main()
 {
-  const unsigned int target = 10;
+  const unsigned int target = 200;
   std::vector<unsigned int> coinSizes = {1, 2, 5, 10, 20, 50, 100, 200};
   std::vector<unsigned long> ways(target+1);
   ways[0] = 1;
