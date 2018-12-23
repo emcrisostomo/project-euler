a9e9108f87f1255947c91f9bb3bd234d7a1a9c4b
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-12-23 01:03:06 +0100

    Update euler-034

--- euler-034.cpp
+++ euler-034.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <utility>
-#include "multiple.h"
+#include <vector>
+#include "factorial.h"
+
+void calculate_factorials(std::vector<unsigned int>& vector, int limit);
+unsigned long sum_of_factorial_digits(unsigned int i, const std::vector<unsigned int>& factorials);
 
 // 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 //
@@ -11,7 +14,37 @@
 //
 int main()
 {
-  std::cout << "\n";
+  std::vector<unsigned int> digit_factorial(10, 0);
+  calculate_factorials(digit_factorial, 9);
+  const unsigned int limit = digit_factorial[9] * 7;
+
+  unsigned long sum{0};
+  for (auto i = 3; i < limit; ++i)
+  {
+    if (i == sum_of_factorial_digits(i, digit_factorial)) sum += i;
+  }
+
+  std::cout << sum << "\n";
 
   return 0;
+}
+
+unsigned long sum_of_factorial_digits(unsigned int i, const std::vector<unsigned int>& factorials)
+{
+  unsigned long sum{0};
+
+  do
+  {
+    sum += factorials[i % 10];
+  } while (i /= 10);
+
+  return sum;
+}
+
+void calculate_factorials(std::vector<unsigned int>& vector, int limit)
+{
+  for (unsigned int i = 0; i <= limit; ++i)
+  {
+    vector[i] = factorial::of(i);
+  }
 }
