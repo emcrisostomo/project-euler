430dad0bdd2541c37e53c61e611b2ce6109c33ae
Crisostomo, Enrico M <enrico.m.crisostomo@accenture.com>
2018-12-26 11:56:48 +0100

    Reformat

--- euler-036.cpp
+++ euler-036.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include "palindrome.h"
 
-//
 // The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
 //
-// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+// Find the sum of all numbers, less than one million, which are palindromic in
+// base 10 and base 2.
 //
-// (Please note that the palindromic number, in either base, may not include leading zeros.)
+// (Please note that the palindromic number, in either base, may not include
+// leading zeros.)
 //
 int main()
 {
   const unsigned int limit = 1000000;
   unsigned long sum{0};
 
-  for (auto i=1; i < limit; ++i)
+  for (auto i = 1; i < limit; ++i)
   {
     if (palindrome::is(i, 2) && palindrome::is(i)) sum += i;
   }
