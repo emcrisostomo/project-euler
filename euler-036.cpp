45aee2e2b9ba16130d2db59ded514502f927fb6c
Crisostomo, Enrico M <enrico.m.crisostomo@accenture.com>
2018-12-26 11:31:33 +0100

    Add skeleton for euler-036

---
+++ euler-036.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "palindrome.h"
+
+bool rotations_are_prime(int prime, std::vector<bool> sieve);
+
+//
+// The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+//
+// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+//
+// (Please note that the palindromic number, in either base, may not include leading zeros.)
+//
+int main()
+{
+
+
+  return 0;
+}
+
