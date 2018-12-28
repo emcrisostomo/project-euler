0f01d63da3a960da32f3c7b4e33c34d983ea9f93
Crisostomo, Enrico M <enrico.m.crisostomo@accenture.com>
2018-12-28 09:57:35 +0100

    Add euler-039.cpp

--- src/001-050/euler-039.cpp
+++ src/001-050/euler-039.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
 // If p is the perimeter of a right angle triangle with integral length sides,
 // {a,b,c}, there are exactly three solutions for p = 120.
@@ -9,9 +11,41 @@
 //
 int main()
 {
+  // We're looking for the integral solutions of:
+  //
+  //   x^2 + y^2 = z^2
+  //   x + y + z = p
   unsigned long max{0};
+  unsigned long max_p{0};
 
-  std::cout << max << "\n";
+  for (unsigned int p = 1; p <= 1000; ++p)
+  {
+    unsigned long solutions{0};
+
+    for (unsigned int x = 1; x <= p / 3; ++x)
+    {
+      for (unsigned int y = x; y < p - x; ++y)
+      {
+        auto z = (p - x - y);
+        if (z < 0) continue;
+
+        auto z2 = (x * x) + (y * y);
+
+        if ((z * z) == z2)
+        {
+          ++solutions;
+        }
+      }
+    }
+
+    if (solutions > max)
+    {
+      max = solutions;
+      max_p = p;
+    }
+  }
+
+  std::cout << max_p << "\n";
 
   return 0;
 }
