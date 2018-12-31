bbbb8bf9c36f435a47ee9dcb0751f49d64c06fa8
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-12-31 17:06:32 +0100

    Add euler-041.cpp

--- src/001-050/euler-041.cpp
+++ src/001-050/euler-041.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "../prime.h"
+#include "../number.h"
 
 // We shall say that an n-digit number is pandigital if it makes use of all the
 // digits 1 to n exactly once.  For example, 2143 is a 4-digit pandigital and is
@@ -8,7 +10,16 @@
 //
 int main()
 {
-  std::cout << "\n";
+  std::vector<bool> sieve = prime::sieve_of_erathostenes(7654321);
 
-  return 0;
+  for (long i = sieve.size() - 1; i >= 0; --i)
+  {
+    if (!sieve[i]) continue;
+    if (!number::is_pandigital(i)) continue;
+
+    std::cout << i << "\n";
+    return 0;
+  }
+
+  return 1;
 }
