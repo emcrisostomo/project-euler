52c96e3ae849a07f0501d5e2488def2007a4db63
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2019-01-01 04:07:35 +0100

    Add euler-042.cpp

--- src/001-050/euler-042.cpp
+++ src/001-050/euler-042.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+bool is_triangular_word(std::string word);
+bool is_triangular(unsigned int number);
 
 // The nth term of the sequence of triangle numbers is given by
 //
@@ -16,11 +22,60 @@
 // Using words.txt, a 16K text file containing nearly two-thousand common
 // English words, how many are triangle words?
 //
-int main()
+int main(int argc, char *argv[])
 {
+  if (argc != 2)
+  {
+    std::cerr << "Invalid number of arguments: " << argc << "\n";
+    return 1;
+  }
+
+  std::fstream infile(argv[1]);
+
+  if (!infile.is_open())
+  {
+    std::cerr << "Cannot open file: " << argv[1] << "\n";
+    return 1;
+  }
+
   unsigned int count{0};
+  std::string line;
+
+  while (std::getline(infile, line, ','))
+  {
+    line = line.substr(1, line.size() - 2);
+    if (is_triangular_word(line)) ++count;
+  }
 
   std::cout << count << "\n";
 
   return 0;
+}
+
+bool is_triangular_word(std::string word)
+{
+  unsigned int value{0};
+
+  for (const auto& c : word)
+  {
+    value += c - 'A' + 1;
+  }
+
+  return is_triangular(value);
+}
+
+bool is_triangular(unsigned int number)
+{
+  //   t_n = n(n+1)/2
+  //
+  // is a second degree equation in n.  Solving it yields only one valid
+  // solution for n > 0:
+  //
+  //   n = (-1 +- sqrt(1+8t))/2
+  //
+  // Since n is integer, sqrt(1+8t) must be integer as well.
+  unsigned int n = 8 * number + 1;
+  auto r = static_cast<unsigned int>(std::sqrt(n));
+
+  return (n == r * r);
 }
