b342f0e73a1872b00763aad06390eafa506c4f14
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2019-01-06 00:10:51 +0100

    Add comments to clarify the algorithm

--- src/001-050/euler-044.cpp
+++ src/001-050/euler-044.cpp
@@ -23,16 +23,34 @@ int main(int argc, char *argv[])
 
   while (true)
   {
+    // P_{n+1} - P_{n} = ((n+1)(3n + 3 - 1) - (n(3n-1)))/2
+    //   = (6n + 2)/2
+    //   = 3n + 1
     previous += (3 * n + 1);
     ++n;
     pentagonal_numbers.push_back(previous);
 
     for (const auto& p : pentagonal_numbers)
     {
-      auto sum = previous - p;
-      auto diff = (sum > p ? sum - p : p - sum);
+      // For each pentagonal number p already processed, let's see whether p can
+      // be considered part of a sum and a difference of pentagonal numbers
+      // whose result is a pentagonal number:
+      //
+      //   P_j = previous - p
+      //   P_k = p
+      //
+      // so that:
+      //
+      //   P_j + P_k = previous
+      //   | P_j - P_k | = | previous - p - p |
+      //
+      // The sum is a pentagonal number because it's the latest generated
+      // number, so that we have to check that both P_j itself and the
+      // difference is pentagonal.
+      auto p_j = previous - p;
+      auto diff = (p_j > p ? p_j - p : p - p_j);
 
-      if (number::is_pentagonal(sum) && number::is_pentagonal(diff))
+      if (number::is_pentagonal(p_j) && number::is_pentagonal(diff))
       {
         std::cout << diff << "\n";
         return 0;
