cf011adc3fca1baa4d4b0149e12b2a29247949b7
Enrico Maria Crisostomo <enrico.m.crisostomo@gmail.com>
2019-02-11 20:53:22 +0100

    Add skeleton of euler-049

--- src/001-050/euler-049.cpp
+++ src/001-050/euler-049.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include "number.h"
 #include "prime.h"
 
 // The arithmetic sequence, 1487, 4817, 8147, in which each of the terms
@@ -15,7 +18,36 @@
 //
 int main(int argc, char *argv[])
 {
+  const unsigned int limit = 9999;
+  const auto sieve = prime::sieve_of_erathostenes(limit);
   unsigned long sequence{0};
+
+  for (unsigned short f = 0; f <= 9; ++f)
+  {
+    for (unsigned short s = f; s <= 9; ++s)
+    {
+      for (unsigned short t = s; t <= 9; ++t)
+      {
+        for (unsigned short fo = t; fo <= 9; ++fo)
+        {
+          std::vector<unsigned short> digits{f, s, t, fo};
+          std::vector<unsigned int> primes;
+          primes.reserve(24);
+
+          do
+          {
+            unsigned int n = number::number_from_digits<unsigned short, unsigned int>(digits);
+            if (sieve[n])
+              primes.push_back(n);
+          } while (std::next_permutation(digits.begin(), digits.end()));
+
+          if (primes.size() < 3)
+            continue;
+        }
+      }
+    }
+  }
+
   std::cout << sequence << "\n";
   return 0;
 }
