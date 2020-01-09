a88dc950ccb20e273b40b365183795c77369f5e8
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-01-09 01:36:46 +0100

    Remove unused included header

--- src/051-100/euler-067.cpp
+++ src/051-100/euler-067.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include <cassert>
 #include "triangle.h"
 
 // By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
