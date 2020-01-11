80dc8e215f201a492623af0117964c512b478646
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-01-11 18:07:02 +0100

    Add euler-056.cpp skeleton

---
+++ src/051-100/euler-056.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// A googol (10^{100}) is a massive number: one followed by one-hundred zeros;
+// 100100 is almost unimaginably large: one followed by two-hundred zeros.
+// Despite their size, the sum of the digits in each number is only 1.
+//
+// Considering natural numbers of the form, a^b, where a, b < 100, what is the
+// maximum digital sum?
+//
+int main(int argc, char *argv[])
+{
+  unsigned long max_sum{0};
+
+  std::cout << max_sum << '\n';
+  return 0;
+}
