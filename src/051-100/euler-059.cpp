3494e0dd7dc6d30380bbddcf39397a61b1235e8c
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-01-18 23:57:38 +0100

    Add euler-059.cpp skeleton

--- src/051-100/euler-059.cpp
+++ src/051-100/euler-059.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cassert>
+#include "number.h"
+
+void parse_bytes(std::string& line, std::vector<char>& bytes);
+void parse_token(const std::string& char_value, std::vector<char>& bytes);
+std::vector<std::vector<char>> split_bytes_in_stacks(const std::vector<char>& bytes);
 
 // Each character on a computer is assigned a unique code and the preferred
 // standard is ASCII (American Standard Code for Information Interchange).  For
@@ -27,9 +35,78 @@
 // English words, decrypt the message and find the sum of the ASCII values in
 // the original text.
 //
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-  std::cout << '\n';
+  if (argc != 2)
+  {
+    std::cerr << "Invalid number of arguments: " << argc << "\n";
+    return 1;
+  }
 
+  std::fstream infile(argv[1]);
+  std::string line;
+  std::vector<char> bytes;
+
+  while (std::getline(infile, line))
+  {
+    parse_bytes(line, bytes);
+  }
+
+  std::vector<std::vector<char>> bytes_stacks = split_bytes_in_stacks(bytes);
+
+  for (auto i = 0; i < 26 * 26 * 26; ++i)
+  {
+    std::vector<unsigned short> digits = number::get_digits(i, 26);
+    digits.resize(3);
+
+    for (const auto& d : digits)
+    {
+      std::cout << static_cast<char>('a' + d);
+    }
+
+    std::cout << '\n';
+  }
+
+  std::cout << 0 << '\n';
   return 0;
+}
+
+std::vector<std::vector<char>>
+split_bytes_in_stacks(const std::vector<char>& bytes)
+{
+  std::vector<std::vector<char>> stacks(3);
+
+  for (auto v : stacks)
+    v.reserve(bytes.size() / 3 + 1);
+
+  for (auto i = 0; i < bytes.size(); ++i)
+    stacks[i % 3].push_back(bytes[i]);
+
+  return stacks;
+}
+
+void
+parse_bytes(std::string& line, std::vector<char>& bytes)
+{
+  // The line is a comma-separated list of char values.
+  const std::string delimiter = ",";
+  size_t pos = 0;
+  std::string token;
+
+  while ((pos = line.find(delimiter)) != std::string::npos)
+  {
+    token = line.substr(0, pos);
+
+    parse_token(token, bytes);
+    line.erase(0, pos + delimiter.length());
+  }
+
+  parse_token(line, bytes);
+}
+
+void
+parse_token(const std::string& char_value, std::vector<char>& bytes)
+{
+  bytes.push_back(static_cast<char>(std::stoi(char_value)));
 }
