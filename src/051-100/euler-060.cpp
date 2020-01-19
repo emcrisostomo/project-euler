a8762aafb812a221c49930f97ac61e5890e918e3
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-01-19 19:02:48 +0100

    Add euler-060.cpp skeleton

---
+++ src/051-100/euler-060.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// The primes 3, 7, 109, and 673, are quite remarkable.  By taking any two
+// primes and concatenating them in any order the result will always be prime.
+// For example, taking 7 and 109, both 7109 and 1097 are prime.  The sum of
+// these four primes, 792, represents the lowest sum for a set of four primes
+// with this property.
+//
+// Find the lowest sum for a set of five primes for which any two primes
+// concatenate to produce another prime.
+//
+int
+main(int argc, char *argv[])
+{
+  unsigned long sum{0};
+
+  std::cout << sum << '\n';
+  return 0;
+}
