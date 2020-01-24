abb4cd9f6189b6ebe46a46b0d68b79b8b01552dc
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2020-01-24 22:10:24 +0100

    Add euler-060+combinations.cpp

---
+++ src/051-100/euler-060+combinations.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <cassert>
+#include <numeric>
+#include <number.h>
+#include <set>
+#include "prime.h"
+#include "combinations.h"
+
+static const int FAMILY_SIZE = 5;
+
+unsigned long find_min_sum_of_family_of_size(const std::vector<unsigned long>& candidates, size_t family_size);
+bool is_concatenated_pair_prime(const unsigned long& f, const unsigned long& s);
+bool is_concatenated_family(const std::vector<unsigned long>& primes, size_t last_changed, size_t& last_match);
+std::vector<unsigned long> get_elements_by_index(const std::vector<unsigned long>& vector, const std::vector<size_t>& indexes);
+unsigned long next_combination(std::vector<size_t>& vector, size_t n, size_t k);
+size_t next_combination_starting_from(std::vector<size_t>& vector, size_t n, size_t k, size_t last_match);
+std::vector<unsigned long> find_pairs_of(const unsigned long& p, size_t index, const std::vector<unsigned long>& primes);
+std::vector<size_t> init_combination(size_t k);
+bool check_concatenated_family(const std::vector<unsigned long>& primes, size_t last_changed);
+
+// The primes 3, 7, 109, and 673, are quite remarkable.  By taking any two
+// primes and concatenating them in any order the result will always be prime.
+// For example, taking 7 and 109, both 7109 and 1097 are prime.  The sum of
+// these four primes, 792, represents the lowest sum for a set of four primes
+// with this property.
+//
+// Find the lowest sum for a set of five primes for which any two primes
+// concatenate to produce another prime.
+//
+int
+main(int argc, char *argv[])
+{
+  std::vector<unsigned long> primes = prime::get_primes_smaller_than(9000);
+
+  // Remove 2 and 5
+  primes.erase(primes.begin());
+  primes.erase(primes.begin() + 1);
+
+  unsigned long sum{std::numeric_limits<unsigned long>::max()};
+
+  for (auto i = 0; i < primes.size(); ++i)
+  {
+    const auto& p = primes[i];
+    const std::vector<unsigned long>& candidate_pairs = find_pairs_of(p, i, primes);
+
+    auto ret = find_min_sum_of_family_of_size(candidate_pairs, FAMILY_SIZE);
+
+    if (ret > 0)
+      sum = std::min(ret, sum);
+  }
+
+  std::cout << sum << '\n';
+  return 0;
+}
+
+std::vector<unsigned long>
+find_pairs_of(const unsigned long& p, size_t index, const std::vector<unsigned long>& primes)
+{
+  std::vector<unsigned long> candidate_pairs;
+  candidate_pairs.push_back(p);
+
+  for (auto j = index + 1; j < primes.size(); ++j)
+  {
+    const auto& s = primes[j];
+    if (is_concatenated_pair_prime(p, s))
+      candidate_pairs.push_back(s);
+  }
+
+  return candidate_pairs;
+}
+
+unsigned long
+find_min_sum_of_family_of_size(const std::vector<unsigned long>& candidates, size_t family_size)
+{
+  if (candidates.size() < family_size)
+    return 0;
+
+  unsigned long ret{std::numeric_limits<unsigned long>::max()};
+  const size_t k = family_size;
+  const size_t n = candidates.size();
+
+  std::vector<size_t> comb_index = init_combination(k);
+  size_t last_changed{0};
+
+  while (comb_index[k - 1] < n)
+  {
+    const std::vector<unsigned long>& prime_family = get_elements_by_index(candidates, comb_index);
+
+    size_t last_match{0};
+
+    if (is_concatenated_family(prime_family, last_changed, last_match))
+    {
+      unsigned long family_sum{std::accumulate(prime_family.begin(), prime_family.end(), 0UL)};
+      ret = std::min(ret, family_sum);
+      last_changed = next_combination(comb_index, n, k);
+    }
+    else
+    {
+      // we have to skip all the combinations that have the first last_match
+      // elements in common
+      last_changed = next_combination_starting_from(comb_index, n, k, last_match);
+    }
+  }
+
+  return ret;
+}
+
+std::vector<size_t>
+init_combination(size_t k)
+{
+  std::vector<size_t> comb_index(k);
+  for (auto i = 0; i < k; ++i)
+    comb_index[i] = i;
+
+  return comb_index;
+}
+
+// TODO: k could be deduced from the vector size
+unsigned long
+next_combination(std::vector<size_t>& vector, size_t n, size_t k)
+{
+  size_t idx = k - 1;
+
+  while (idx != 0 && vector[idx] == n - k + idx)
+    --idx;
+
+  const size_t changed_index = idx;
+  ++vector[idx];
+
+  for (auto i = idx + 1; i < k; ++i)
+    vector[i] = vector[i - 1] + 1;
+
+  return changed_index;
+}
+
+size_t
+next_combination_starting_from(std::vector<size_t>& vector,
+                               const size_t n,
+                               const size_t k,
+                               size_t last_match)
+{
+  if (last_match >= vector.size())
+    throw std::runtime_error("last_match out of range");
+
+  size_t idx = k - 1;
+
+  while (idx != 0 && vector[idx] == n - k + idx)
+    --idx;
+
+  if (idx > last_match)
+    idx = last_match;
+
+  const size_t changed_index = idx;
+  ++vector[idx];
+
+  for (auto i = idx + 1; i < k; ++i)
+    vector[i] = vector[i - 1] + 1;
+
+  return changed_index;
+}
+
+std::vector<unsigned long>
+get_elements_by_index(const std::vector<unsigned long>& vector, const std::vector<size_t>& indexes)
+{
+  std::vector<unsigned long> ret;
+  ret.reserve(indexes.size());
+
+  for (const auto& i : indexes)
+    ret.push_back(vector[i]);
+
+  return ret;
+}
+
+bool
+is_concatenated_family(const std::vector<unsigned long>& primes, size_t last_changed, size_t& last_match)
+{
+  if (primes.size() < 2)
+    throw std::runtime_error("Invalid size");
+
+  if (last_changed >= primes.size())
+    throw std::runtime_error("last changed index not in range");
+
+  for (auto i = last_changed; i < primes.size(); ++i)
+    if (!check_concatenated_family(primes, i))
+    {
+      last_match = i;
+      return false;
+    }
+
+  return true;
+}
+
+bool check_concatenated_family(const std::vector<unsigned long>& primes, size_t last_changed)
+{
+  static std::set<std::pair<unsigned long, unsigned long>> discarded_pairs;
+  static std::set<std::pair<unsigned long, unsigned long>> concatenated_pairs;
+
+  // This function assumes that the family in the range f = [0, last_changed[ is
+  // a concatenated family.  Elements in the [last_changed, primes.size()[
+  // range will be checked against the f family one by one.
+  std::vector<unsigned long> verified_family;
+  verified_family.reserve(last_changed);
+
+  for (auto i = 0; i < last_changed; ++i)
+    verified_family.push_back(primes[i]);
+
+  const auto& not_verified = primes[last_changed];
+
+  for (const auto& verified : verified_family)
+  {
+    const auto pair = std::make_pair(verified, not_verified);
+
+    if (discarded_pairs.find(pair) != discarded_pairs.end())
+      return false;
+
+    if (concatenated_pairs.find(pair) != concatenated_pairs.end())
+      continue;
+
+    if (!is_concatenated_pair_prime(pair.first, pair.second))
+    {
+      discarded_pairs.insert(pair);
+      return false;
+    }
+
+    concatenated_pairs.insert(pair);
+  }
+
+  return true;
+}
+
+bool
+is_concatenated_pair_prime(const unsigned long& f, const unsigned long& s)
+{
+  // TODO: profile usage of a sieve vs.
+//  static const std::vector<bool>& sieve =
+//    prime::sieve_of_erathostenes(100000000);
+
+  unsigned long left_candidate = (f * static_cast<unsigned long>(std::pow(10, number::digits_in_number(s))) + s);
+  unsigned long right_candidate = (s * static_cast<unsigned long>(std::pow(10, number::digits_in_number(f))) + f);
+
+  return prime::is_prime(left_candidate) && prime::is_prime(right_candidate);
+//  return sieve[left_candidate] && sieve[right_candidate];
+}
--- src/051-100/euler-060.cpp
+++ src/051-100/euler-060.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <cassert>
+#include <numeric>
+#include <number.h>
+#include <set>
+#include "prime.h"
+#include "combinations.h"
+
+static const int FAMILY_SIZE = 4;
+
+std::vector<bool> init_primes_mask(const std::vector<unsigned long>& primes);
+bool is_concatenated_family(const std::vector<unsigned long>& primes);
+bool is_concatenated_pair_prime(const unsigned long& f, const unsigned long& s);
 
 // The primes 3, 7, 109, and 673, are quite remarkable.  By taking any two
 // primes and concatenating them in any order the result will always be prime.
@@ -12,8 +24,80 @@
 int
 main(int argc, char *argv[])
 {
+  // I've tried an approach where permutations where
+  std::vector<unsigned long> primes = prime::get_primes_smaller_than(1000);
+  // Remove 2 and 5
+  primes.erase(primes.begin());
+  primes.erase(primes.begin() + 1);
+
   unsigned long sum{0};
+  std::vector<bool> primes_mask = init_primes_mask(primes);
+
+  do
+  {
+    const std::vector<unsigned long>& masked_primes = combinations::get_masked_elements(primes, primes_mask);
+
+    if (is_concatenated_family(masked_primes))
+    {
+      sum = std::accumulate(masked_primes.begin(), masked_primes.end(), sum);
+      break;
+    }
+  } while (std::next_permutation(primes_mask.rbegin(), primes_mask.rend()));
 
   std::cout << sum << '\n';
   return 0;
+}
+
+bool
+is_concatenated_family(const std::vector<unsigned long>& primes)
+{
+  static std::set<std::pair<unsigned long, unsigned long>> discarded_pairs;
+
+  if (primes.size() < 2)
+    throw std::runtime_error("Invalid size");
+
+  std::vector<bool> mask(primes.size());
+  mask[0] = true;
+  mask[1] = true;
+
+  do
+  {
+    const std::vector<unsigned long>& prime_pair = combinations::get_masked_elements(primes, mask);
+    const auto& f = prime_pair[0];
+    const auto& s = prime_pair[1];
+
+    if (discarded_pairs.find(std::make_pair(f, s)) != discarded_pairs.end())
+      return false;
+
+    if (!is_concatenated_pair_prime(f, s))
+    {
+      discarded_pairs.insert(std::make_pair(f, s));
+      return false;
+    }
+  } while (std::next_permutation(mask.rbegin(), mask.rend()));
+
+  return true;
+}
+
+bool
+is_concatenated_pair_prime(const unsigned long& f, const unsigned long& s)
+{
+  static const std::vector<bool>& sieve =
+    prime::sieve_of_erathostenes(10000000);
+  unsigned long left_candidate = (f * static_cast<unsigned long>(std::pow(10, number::digits_in_number(s))) + s);
+  unsigned long right_candidate = (s * static_cast<unsigned long>(std::pow(10, number::digits_in_number(f))) + f);
+
+  return sieve[left_candidate] && sieve[right_candidate];
+}
+
+std::vector<bool>
+init_primes_mask(const std::vector<unsigned long>& primes)
+{
+  assert(primes.size() >= FAMILY_SIZE);
+
+  std::vector<bool> primes_mask(primes.size());
+  for (int i = 0; i < FAMILY_SIZE; ++i)
+    primes_mask[i] = true;
+
+  return primes_mask;
 }
